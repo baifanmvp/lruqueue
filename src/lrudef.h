2165b21f2dc1ab28972591ce3de075c72f572f32
baifanmvp <baifanmvp@gmail.com>
2013-03-08 18:25:46 +0800

    1

--- src/lrudef.h
+++ src/lrudef.h
@@ -1,4 +1,10 @@
 #ifndef _LRU_DEF_H
 #define _LRU_DEF_H
 #include <stddef.h>
+#include <inttypes.h>
+typedef int lbool;
+#define LRU_NULL     0
+
+#define LRU_FALSE     0
+#define LRU_TRUE      1
 #endif
